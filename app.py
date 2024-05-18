423aec766f9401eee7d22fe07c27e1c9e30fce3a
gabrielawaz00 <gabrielawaz00@gmail.com>
2024-05-18 13:15:01 +0200

    Add Flask app with Dockerfile

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from sklearn.linear_model import Perceptron
+import numpy as np
 
 # Create a flask
 app = Flask(__name__)
 
-# Create an API end point
-@app.route('/')
-def say_hello():
-    return "Hello World"
+X = np.array([[0, 0], [1, 1], [1, 0], [0, 1]])
+y = np.array([0, 1, 1, 0])
+
+model = Perceptron()
+model.fit(X, y)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    features = np.array([data['features']])
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
     app.run() # domyślnie ustawia localhost i port 5000
